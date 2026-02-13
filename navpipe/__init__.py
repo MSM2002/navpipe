221919f0d563e09122dc712df90a9a906e20da80
MSM2002 <manaswi.m02@gmail.com>
2026-02-13 22:41:34 +0530

    build: Fix subpackage discovery error.

--- navpipe/__init__.py
+++ navpipe/__init__.py
@@ -4,7 +4,7 @@ import importlib.metadata
 try:
     __version__ = importlib.metadata.version("navpipe")
 except importlib.metadata.PackageNotFoundError:
-    __version__ = "0.1.0"
+    __version__ = "0.1.1"
 
 
 __all__ = ["NavPipe"]
