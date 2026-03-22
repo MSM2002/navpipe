d49795511dc094a3cb7221cb03fb03a53a100e84
MSM2002 <manaswi.m02@gmail.com>
2026-03-22 14:55:10 +0530

    build: Update polars version to allow abi3-py313

--- src/transform/nav.rs
+++ src/transform/nav.rs
@@ -5,20 +5,23 @@ use polars::prelude::*;
 pub fn nav_response_to_df(resp: &NavResponse) -> Result<DataFrame, PolarsError> {
     let meta = &resp.meta;
     let dates: Vec<&str> = resp.data.iter().map(|x| x.date.as_str()).collect();
-    let height = dates.len(); 
-    
-    let navs: Result<Vec<f64>, _> = resp
-        .data
-        .iter()
-        .map(|x| x.nav.parse::<f64>())
-        .collect();
+    let height = dates.len();
+
+    let navs: Result<Vec<f64>, _> = resp.data.iter().map(|x| x.nav.parse::<f64>()).collect();
 
     let navs = navs.map_err(|e| PolarsError::ComputeError(e.to_string().into()))?;
 
     let df = DataFrame::new(
+        height,
         vec![
-            Column::from(Series::new("scheme_code".into(), vec![meta.scheme_code; height])),
-            Column::from(Series::new("scheme_name".into(), vec![meta.scheme_name.clone(); height])),
+            Column::from(Series::new(
+                "scheme_code".into(),
+                vec![meta.scheme_code; height],
+            )),
+            Column::from(Series::new(
+                "scheme_name".into(),
+                vec![meta.scheme_name.clone(); height],
+            )),
             Column::from(Series::new("date".into(), dates)),
             Column::from(Series::new("nav".into(), navs)),
         ],
